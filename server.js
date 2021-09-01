42797c442d6fee1f47832e420771349d24e885e6
turner749 <turner.k.alex@gmail.com>
2021-09-01 00:56:05 -0400

    fix server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from "express";
 import path from "path";
 
 const app = express();
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 app.use(serveStatic(path.join(__dirname, "dist")));
 const port = process.env.PORT || 80;
 app.listen(port);
